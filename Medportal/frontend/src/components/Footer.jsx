b023eaba9e193fc09c38a428d829d5e13deeebfe
blakep7 <pearsonblake9@gmail.com>
2024-04-24 19:35:46 -0700

    Update footer to match theme
    Signed-off-by: blakep7 <pearsonblake9@gmail.com>

--- Medportal/frontend/src/components/Footer.jsx
+++ Medportal/frontend/src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <>  
-    <footer className="footer mt-auto bg-dark text-light py-4">
+    <footer className="footer mt-auto bg-light text-dark py-4">
         <div className="container text-center">
           <p>&copy; 2024 Med-Portal</p>
           <ul className="list-inline">
-            <li className="list-inline-item"><a className="text-light" href="#">Privacy</a></li>
-            <li className="list-inline-item"><a className="text-light" href="#">Terms</a></li>
-            <li className="list-inline-item"><a className="text-light" href="#">Support</a></li>
+            <li className="list-inline-item"><a className="text-dark" href="#">Privacy</a></li>
+            <li className="list-inline-item"><a className="text-dark" href="#">Terms</a></li>
+            <li className="list-inline-item"><a className="text-dark" href="#">Support</a></li>
           </ul>
         </div>
       </footer>
