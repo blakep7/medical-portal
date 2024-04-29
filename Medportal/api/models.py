2441a3ecc3ab8bf1ed97457b9538a7079c4dd99e
Blake Pearson <91240051+blakep7@users.noreply.github.com>
2024-04-29 15:59:59 -0700

    Prescription Page Update, New User Information on Back-End, Sign-Up page update. (#43)
    * Update footer to match theme
    
    Signed-off-by: blakep7 <pearsonblake9@gmail.com>
    
    * Update prescription pages to match theme, add user fields for information like height weight etc.
    
    Signed-off-by: blakep7 <pearsonblake9@gmail.com>
    
    * Update auth checking
    
    Signed-off-by: blakep7 <pearsonblake9@gmail.com>
    
    ---------
    
    Signed-off-by: blakep7 <pearsonblake9@gmail.com>

--- Medportal/api/models.py
+++ Medportal/api/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name,  user_type, password=None):
+    def create_user(self, email, first_name, last_name, height_in, weight_lb, DOB, phone_number, user_type, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, user_type=user_type)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, user_type=user_type, height_in=height_in, weight_lb=weight_lb, DOB=DOB, phone_number=phone_number)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -21,11 +21,19 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     user_type = models.CharField(max_length=10)
+    
+    height_in = models.IntegerField(default=0)
+    weight_lb = models.IntegerField(default=0)
+    physician = models.CharField(max_length=255, default='Dr. Benjamin Park')
+    DOB = models.DateField(default='2000-01-01')
+    phone_number = models.CharField(max_length=255, default='(123) 456-7890')
+    profile_picture_ref = models.CharField(max_length=255, default='default.jpg')
+    
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'user_type']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'user_type', 'height_in', 'weight_lb', 'DOB', 'phone_number', 'physician', 'profile_picture_ref']
 
     def get_first_name(self):
         return self.first_name
@@ -38,6 +46,8 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.user_type
+    
+    
        
 class Drug(models.Model):
     active_ingrediant = models.CharField(max_length=255)
--- Medportal/frontend/src/components/PatientCard.jsx
+++ Medportal/frontend/src/components/PatientCard.jsx
@@ -1,19 +1,21 @@
+import defaultPic from '../assets/default.jpg';
+
 const PatientCard = (props) => {
 
     return (
         <>
-            <img src="../assets/temp_profile_pic.jpg" alt="Selected User Profile Picture" className="img-thumbnail square mr-3" style={{ width: '100px', height: '150px' }}/>
-            <div className="row align-items-end">
+            <img src={defaultPic} alt="Selected User Profile Picture" className="img-thumbnail square mr-3" style={{ width: '100px', height: '150px' }}/>
+            <div className="row align-items-end flex-fill">
                 <div className="col-sm-6 text-start justify-content-start">
                     <h1 className="px-3">{props.first_name} {props.last_name}</h1>
-                    <h5 className="px-3">Height: 5' 6"</h5>
-                    <h5 className="px-3">Weight: 120lbs - 210kg</h5>
-                    <h5 className="px-3">Physician: Dr. Michael Bradshaw</h5>
+                    <h5 className="px-3">Height: {props.height} in</h5>
+                    <h5 className="px-3">Weight: {props.weight} lbs</h5>
+                    <h5 className="px-3">Physician: {props.physician} </h5>
                 </div>
                 <div className="col-sm-6 text-start justify-content-end">
-                    <h5 className="px-3">DOB: 03/11/2003</h5>
+                    <h5 className="px-3">DOB: {props.DOB} </h5>
                     <h5 className="px-3">Email: {props.email}</h5>
-                    <h5 className="px-3">Phone Number: 1 (619) 555-5555</h5>
+                    <h5 className="px-3">Phone Number: {props.phone_number}</h5>
                 </div>
             </div>
         </>
